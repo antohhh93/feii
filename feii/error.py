6710744e5ec7190776862481f709d5b0152cb890
Турко Антон Мустафаевич <turko@skbkontur.ru>
2021-01-11 17:02:28 +0500

    Add new fix shrink index

--- feii/error.py
+++ feii/error.py
@@ -98,6 +98,12 @@ class FixError(Structure):
       if index['error.type'] == "process_cluster_event_timeout_exception":
         self.not_ilm_retry_index_and_check()
 
+      if index['error.type'] == "index_closed_exception":
+        self.not_ilm_retry_index_and_check()
+
+      if index['error.type'] == "null_pointer_exception":
+        self.not_ilm_retry_index_and_check()
+
   def fix_error_ilm_last_index_in_check_mode(self):
     for index in self.error_ilm_last_indices:
       self.logger.warning("[check_mode] Fix ilm error [{0}] for index [{1}]".format( index['error.type'], index['index'] ))
