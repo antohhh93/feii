b09a4efcfb88b38bad0fa4ceb766265390e55246
Турко Антон Мустафаевич <turko@skbkontur.ru>
2022-09-08 10:29:31 +0500

    Add store size for closed indexes

--- feii/close.py
+++ feii/close.py
@@ -27,10 +27,11 @@ class Close(Structure):
     self.creating_array_indexes_expired_policy_to_close()
 
     for index in self.list_indices_to_close:
-      self.logger.warning("[check_mode] Closing expired ilm policy index [{0}] age exceeds - [{1}].".format( index['index'], index['policy.age'] ))
+      self.logger.warning("[check_mode] Closing expired ilm policy index [{0}], age exceeds - [{1}], size - {2}Gb.".format( index['index'], index['policy.age'], index['store.size'] ))
 
   def close_indexes_large_age(self):
     for index in self.list_indices_to_close:
       self.index = index['index']
       if self.not_close_index_and_check() != False:
+        self.logger.info("Closing expired ilm policy index [{0}], age exceeds - [{1}], size - {2}Gb.".format( index['index'], index['policy.age'], index['store.size'] ))
         Config.ilm_list['indices'].pop(index['index'])
